b268fab51b92adfd59790ed1809f88e1f67f5b3d
M Anzal Javed <anzaljoffl@gmail.com>
2025-12-22 16:25:21 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center">📝 MERN Stack Note Taking App ✨</h1>
+
+Highlights:
+
+- 🧱 Full-Stack App Built with the MERN Stack (MongoDB, Express, React, Node)
+- ✨ Create, Update, and Delete Notes with Title & Description
+- 🛠️ Fully Functional REST API
+- ⚙️ Rate Limiting with Upstash Redis
+- 🚀 Responsive UI
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router";
+import { PlusIcon } from "lucide-react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-base-300 border-b border-base-content/10">
+      <div className="mx-auto max-w-6xl p-4">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">ThinkNote</h1>
+          <div className="flex items-center gap-4">
+            <p>© 2025 ThinkNote By Anzal. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
