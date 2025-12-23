69d00116f5f7deb442d1dc83d60d910d34539d7a
M Anzal Javed <anzaljoffl@gmail.com>
2025-12-23 20:00:48 +0530

    Style Update

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -23,7 +23,7 @@ const NoteCard = ({ note, setNotes }) => {
   return (
     <Link
       to={`/note/${note._id}`}
-      className="card bg-base-100 hover:shadow-lg transition-all duration-200 
+      className="card bg-base-100 shadow-md hover:shadow-lg transition-all duration-200 
       border-t-4 border-solid border-primary"
     >
       <div className="card-body">
